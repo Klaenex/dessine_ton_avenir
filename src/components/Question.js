35b027b05f3b3adc77c30bbbd2bd6d5d5a223220
klaenex <hello@cuozzovincenzo.be>
2024-01-03 11:17:45 +0100

    css+responsive

--- src/components/Question.js
+++ src/components/Question.js
@@ -5,9 +5,11 @@ function Question() {
   // Affichage des questions et réponses
   const displayQuestions = Data.questions.map((questionItem, index) => (
     <div key={index} className="question-item">
-      <h2>{questionItem.question}</h2>
-      <p>{questionItem.reponse}</p>
-      <div>
+      <div className="wrapper wrapper--text">
+        <h2 className="title">{questionItem.question}</h2>
+        <p>{questionItem.reponse}</p>
+      </div>
+      <div className="wrapper wrapper--img">
         <img
           src={process.env.PUBLIC_URL + "/images/" + questionItem.image}
           alt={questionItem.image_alt}
@@ -17,9 +19,13 @@ function Question() {
   ));
 
   return (
-    <div className="container">
-      <div className="questions-content">{displayQuestions}</div>
-    </div>
+    <>
+      <h2 className="title title--big">Dessine ton avenir</h2>
+
+      <div className="container">
+        <div className="questions-content">{displayQuestions}</div>
+      </div>
+    </>
   );
 }
 
